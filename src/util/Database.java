eb22093c35ee0617d41fc6a111b4981e0622cb18
Scott <yeunghl@whu.edu.cn>
2018-07-03 17:17:51 +0800

    in order to merge

--- src/util/Database.java
+++ src/util/Database.java
@@ -41,4 +41,35 @@ public class Database {
             return false;
         }
     }
+
+    /**
+     * @ author: 叶晟柯
+     * @ date: 2018/7/3 16.24
+     */
+    public List<Product> searchProduct(String s){
+        segmenter segmt = new segmenter();
+        List <String> nouns = segmt.seg(s);
+        String sql = "select * from product where name like '";
+        sql = sql+"%"+nouns.get(0)+"%'";
+        for (int i=1;i<nouns.size();i++){
+            sql = sql+"or name like '%"+nouns.get(i)+"%'";
+        }
+        List<Product> list = new ArrayList<>();
+        try {
+
+            PreparedStatement stmt = connect.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                Product pro = new Product(Integer.getInteger(rs.getString(1)),Integer.getInteger(rs.getString(4)),
+                        Integer.getInteger(rs.getString(6)),Integer.getInteger(rs.getString(7)),
+                        Integer.getInteger(rs.getString(8)),rs.getString(2),
+                        rs.getString(3),rs.getString(5));
+                list.add(pro);
+            }
+        }
+        catch (Exception e)
+        {}
+        return list;
+    }
+
 }
