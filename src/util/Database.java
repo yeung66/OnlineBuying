04c6baf133091818f0d54004343cae93e12d313d
葉落丶莫相離 <446258949@qq.com>
2018-07-03 17:02:00 +0800

    Upload Database.java

--- src/util/Database.java
+++ src/util/Database.java
@@ -4,10 +4,10 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import segmenter.*;
 
 /**
  * @ author: 杨浩麟
@@ -30,15 +30,34 @@ public class Database {
     public Connection getConnect() {
         return connect;
     }
-
-    public boolean checkExist(String sql){
-        try {
-            Statement st = connect.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            return rs.next();
-        }catch (SQLException e){
-            e.printStackTrace();
-            return false;
+    /**
+     * @ author: 叶晟柯
+     * @ date: 2018/7/3 16.24
+     */
+    public List<Product> searchProduct(String s){
+        segmenter segmt = new segmenter();
+        List <String> nouns = segmt.seg(s);
+        String sql = "select * from product where name like '";
+        sql = sql+"%"+nouns.get(0)+"%'";
+        for (int i=1;i<nouns.size();i++){
+            sql = sql+"or name like '%"+nouns.get(i)+"%'";
         }
+        List<Product> list = new ArrayList<>();
+        try {
+
+            PreparedStatement stmt = connect.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                Product pro = new Product(Integer.getInteger(rs.getString(1)),Integer.getInteger(rs.getString(4)),
+                        Integer.getInteger(rs.getString(6)),Integer.getInteger(rs.getString(7)),
+                        Integer.getInteger(rs.getString(8)),rs.getString(2),
+                        rs.getString(3),rs.getString(5));
+                list.add(pro);
+            }
+        }
+        catch (Exception e)
+        {}
+        return list;
     }
+
 }
