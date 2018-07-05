d58e6f21e5d4477c8f53a40b66ec0cf5f662f7f6
kelsier <2269766985@qq.com>
2018-07-05 15:36:13 +0800

    Upload CommentServlet.java

---
+++ src/server/CommentServlet.java
@@ -0,0 +1,51 @@
+package server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.Database;
+
+/**
+ * Servlet implementation class CommentServlet
+ */
+@WebServlet("/comment")
+public class CommentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CommentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int score = Integer.parseInt(request.getParameter("rating"));
+		String content = request.getParameter("content");
+		int product = (int) request.getSession().getAttribute("orderId");
+		String purchaser = request.getSession().getAttribute("purchaser").toString();
+		String sql = "INSERT INTO comment (id, content, product, purchaser, score) VALUES (, '" + content + "', '"
+				+ product + "', '" + purchaser + "', '"+score+"');" ;
+		Database.update(sql);
+	}
+
+}
