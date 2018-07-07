dfea4f92601dabb4d3c4bd5dc9240c85571b42e1
kelsier <2269766985@qq.com>
2018-07-07 10:34:14 +0800

    更新 ChangeUserServlet.java

--- src/server/ChangeUserServlet.java
+++ src/server/ChangeUserServlet.java
@@ -8,26 +8,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /*
 * MADE BY:叶晟柯
  */
-@WebServlet(name = "ChangeUserServlet",urlPatterns = "/ChangeUserServlet")
+@WebServlet(name = "ChangeUserServlet", urlPatterns = "/ChangeUserServlet")
 public class ChangeUserServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-           String pwd = request.getParameter("password");
-           String info = request.getParameter("information");
-           String tel = request.getParameter("telephone");
-           String add = request.getParameter("address");
-           String sex = request.getParameter("sex");
-           String id = request.getParameter("id");//传递用户名给后台数据库处理
-           int i=Database.update("update user set pwd =\""+pwd+"\" "+"where id = \""+id+"\"");
-               i=Database.update("update user set info =\""+info+"\" "+"where id = \""+id+"\"");
-               i=Database.update("update user set tel =\""+tel+"\" "+"where id = \""+id+"\"");
-               i=Database.update("update user set add =\""+add+"\" "+"where id = \""+id+"\"");
-               i=Database.update("update user set sex =\""+sex+"\" "+"where id = \""+id+"\"");
-    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String pwd = request.getParameter("pwd");
+		String info = request.getParameter("info");
+		String tel = request.getParameter("tel");
+		String add = request.getParameter("add");
+		String sex = request.getParameter("sex");
+		String id = request.getParameter("id");
+		String uid = (String) request.getSession().getAttribute("uid");
+		String sql = "SELECT * FROM users WHERE id = '" + id + "';";
+		if(Database.checkExist(sql)) {
+			response.getWriter().write("fail");
+			return;
+		}
+		int i = Database.update("update user set id =\"" + id + "\" " + "where id = \"" + uid + "\"");
+		i = Database.update("update user set pwd =\"" + pwd + "\" " + "where id = \"" + id + "\"");
+		i = Database.update("update user set info =\"" + info + "\" " + "where id = \"" + id + "\"");
+		i = Database.update("update user set tel =\"" + tel + "\" " + "where id = \"" + id + "\"");
+		i = Database.update("update user set add =\"" + add + "\" " + "where id = \"" + id + "\"");
+		i = Database.update("update user set sex =\"" + sex + "\" " + "where id = \"" + id + "\"");
+		response.getWriter().write("success");
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			doGet(request, response);
+	}
 }
