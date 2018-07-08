d062f1cef8ebbad2655070c696c190215c2c48f9
kelsier <2269766985@qq.com>
2018-07-08 19:32:03 +0800

    更新 BuyProductServlet.java

--- src/server/BuyProductServlet.java
+++ src/server/BuyProductServlet.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
@@ -41,7 +42,9 @@ public class BuyProductServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.print("123");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter  out = response.getWriter();
+		out.print("<meta   http-equiv='Content-Type'   content='text/html;   charset=UTF-8'>");   
 		Double money=0.0;
 		int product = Integer.parseInt(request.getParameter("pid"));
 		Double price = Product.getProductInfo(product).getPrice();
@@ -49,7 +52,6 @@ public class BuyProductServlet extends HttpServlet {
 		String purchaser = (String) request.getSession().getAttribute("uid");
 		String sql = "SELECT money FROM users WHERE id = '" + purchaser + "';";
 		Connection connect=Database.getConnect();
-		System.out.print("234");
         try {
             Statement st = connect.createStatement();
             ResultSet rs = st.executeQuery(sql);
@@ -60,22 +62,27 @@ public class BuyProductServlet extends HttpServlet {
             e.printStackTrace();
             return;
         }
-		System.out.print("345");
         if(money < price * quantity) {
-			response.getWriter().print("failed");
+    		out.print("<script>");
+    		out.print("alert('购买失败!');");
+    		out.print("</script>");
+    		out.close();
         	return;
         }else {
         	money -= price * quantity;
-        	sql = "UPDATE user SET money = " + money + "WHERE id = '" + purchaser + "';";
+        	sql = "UPDATE users SET money = '" + money + "' WHERE id = '" + purchaser + "';";
         	Database.update(sql);
         }
-		System.out.print("456");
 		String states = "new";
 		Date starttime = new Date(System.currentTimeMillis());
 		sql = "INSERT INTO orders (purchaser, product, states, quantity, starttime) VALUES ('" + purchaser + "','" + product + "','" 
 				+ states + "','" + quantity + "','" + starttime + "');";
 		Database.update(sql);
-		response.getWriter().print("s");
+		out.print("<script>");
+		out.print("alert('购买成功!');");
+		out.print("window.location.href='perInfo.jsp'");
+		out.print("</script>");
+		out.close();
 	}
 
 }
