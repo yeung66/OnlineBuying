1117c3e142d3be45f28d5a908970d2c0fe6e451c
Scott <yeunghl@whu.edu.cn>
2018-07-09 11:46:00 +0800

    create entity class shoppingcart

--- src/vo/ShoppingCart.java
+++ src/vo/ShoppingCart.java
@@ -15,28 +15,70 @@ import java.util.Date;
 public class ShoppingCart {
     private User user;
     private  Product product;
-    int num;
-    Date startTime;
+    private int num;
+    private Date startTime;
+
+    public ShoppingCart(String uid,String pid,int num,Date startTime){
+        setUser(uid);
+        setProduct(pid);
+        this.num=num;
+        this.startTime=startTime;
+    }
 
     public User getUser(){return user;}
 
     public void setUser(String uid){
         Connection conn = Database.getConnect();
         String sql = "select * from users where id='"+uid+"';";
-
         try{
             Statement st=conn.createStatement();
             //System.out.println(sql);
             ResultSet rs =  st.executeQuery(sql);
             if(rs.next()){
-                User u = new User(rs.getString("id"),rs.getString("pwd"),rs.getString("info"),
+                user = new User(rs.getString("id"),rs.getString("pwd"),rs.getString("info"),
                         rs.getString("addr"),rs.getString("tel"),rs.getString("sex"),
                         rs.getString("rights"),rs.getDouble("money"));
-                //return u;
+
             }
         }catch (SQLException e){
             e.printStackTrace();
         }
-        //return null;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(String pid){
+        Connection conn = Database.getConnect();
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from product where id=" + pid);
+            if (rs.next()) {
+                product = new Product(rs.getInt("id"), rs.getDouble("price"), rs.getInt("num"), rs.getDouble("score"),
+                        rs.getInt("comnum"), rs.getString("name"), rs.getString("owner"), rs.getString("path"),
+                        rs.getString("information"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
     }
 }
